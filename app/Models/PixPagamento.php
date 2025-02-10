6312768a24e680cecf9ce8ca165a72ae9f13ec22
Apollo <149687810+Apollojk106@users.noreply.github.com>
2025-02-10 18:51:50 -0300

    Update PixPagamento.php

--- app/Models/PixPagamento.php
+++ app/Models/PixPagamento.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class PixPagamento extends Model
 {
-    //
+    protected $fillable = [
+        'nome',
+        'telefone',
+        'email',
+        'id_pedido',
+        'valor_total',
+        'pix_key',
+        'pix_code',
+        'status',
+    ];
 }
