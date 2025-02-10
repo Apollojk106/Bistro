c5ed528f86da5ee19dda7e0d0a0d3ec886d0b775
Apollo <149687810+Apollojk106@users.noreply.github.com>
2025-02-10 18:51:21 -0300

    Update ItensPedido.php

--- app/Models/ItensPedido.php
+++ app/Models/ItensPedido.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ItensPedido extends Model
 {
-    //
+    protected $fillable = [
+        'id_pedido',
+        'id_cardapio',
+        'quantidade',
+        'valor_unitario',
+        'subtotal',
+    ];
+
+    protected $casts = [
+        'valor_unitario' => 'decimal:2',
+        'subtotal' => 'decimal:2',
+    ];
 }
