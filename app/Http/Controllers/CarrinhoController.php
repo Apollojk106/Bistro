750280b30ead53fd60dcbd76f5768547f67826ce
Apollojk106 <jk586622@gmail.com>
2025-03-30 17:14:23 -0300

    Tela de Opcao e minimo para o login

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -63,6 +63,34 @@ class CarrinhoController extends Controller
         return view("user.Item", compact('Item', 'Itens', 'Bebidas'));
     }
 
+    public function SalvarSelecao($opcaoSelecionada, $tipoOpcao)
+    {
+        $Opcoes = [
+            'categoria' => $tipoOpcao,
+            'opcao_entrega' => $opcaoSelecionada,
+        ];
+
+        return $this->EditarSelecao($Opcoes);
+    }
+
+    public function SalvarSelecaoHorario($opcaoSelecionada, $tipoOpcao, $horario)
+    {
+        $Opcoes = [
+            'categoria' => $tipoOpcao,
+            'opcao_entrega' => $opcaoSelecionada,
+            'horario' => $horario,
+        ];
+
+        return $this->EditarSelecao($Opcoes);
+    }
+
+    public function EditarSelecao($Opcoes)
+    {
+        session(['Opcoes' => $Opcoes]);
+
+        return redirect()->route('User.OpcaoPedido');
+    }
+
     public function SalvarSacola(Request $request)
     {
         $itens = $request->input('itens', []);
@@ -77,7 +105,7 @@ class CarrinhoController extends Controller
 
         session(['carrinho' => $carrinho]);
 
-        return view("user.FormaDePagamento");
+        return redirect()->route('User.Selecao');
     }
 
     public function SalvarPedido(Request $request)
@@ -140,5 +168,23 @@ class CarrinhoController extends Controller
         session(['carrinho' => $carrinho]);
 
         return response()->json(['success' => true]);
+    }
+
+    public function calcularPedido()
+    {
+        $carrinho = session('carrinho', []);
+
+        $totalItens = 0;
+        $valorTotal = 0;
+
+        foreach ($carrinho as $item) {
+            $totalItens += $item['quantidade'];
+            $valorTotal += $item['quantidade'] * $item['valor'];
+        }
+
+        return [
+            'valor' => $valorTotal,
+            'quantidade' => $totalItens
+        ];
     }
 }
